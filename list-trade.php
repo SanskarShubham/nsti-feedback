d09943dfbd918b1fd7688fdab34c9318fc9db7bc
Karan3404 <karansahu.nsti@gmail.com>
2025-06-04 16:27:35 +0530

    added trade crud basic

--- list-trade.php
+++ list-trade.php
@@ -2,7 +2,7 @@
 
 
 // Query to fetch all rows
-$sql = "SELECT * FROM admin";
+$sql = "SELECT * FROM trade";
 $result = mysqli_query($conn, $sql);
 
 if (!$result) {
@@ -14,20 +14,18 @@ if (!$result) {
 <!-- content -->
 <div class="container-fluid">
     <div class="card-header text-right">
-        <a href="add-admin.php" class="btn btn-primary"><i class="fa fa-plus"></i> Add Admin</a>
+        <a href="add-trade.php" class="btn btn-primary"><i class="fa fa-plus"></i> Add trade</a>
     </div>
     <div class="card mb-3">
 
         <div class="card-body">
-            <h4 class="card-title">Admin List</h4>
+            <h4 class="card-title">Trade List</h4>
             <div class="table-responsive">
                 <table class="table table-bordered table-striped verticle-middle">
                     <thead>
                         <tr>
                             <th scope="col">Id</th>
-                            <th scope="col">Name</th>
-                            <th scope="col">Mobile</th>
-                            <th scope="col">Email</th>
+                            <th scope="col">Trade Name</th>
                             <th scope="col">Status</th>
                             <th scope="col">Action</th>
                         </tr>
@@ -38,19 +36,17 @@ if (!$result) {
                         while ($row = mysqli_fetch_assoc($result)) {
                             echo "<tr>";
                             echo "<td>" . htmlspecialchars($row['id']) . "</td>";
-                            echo "<td>" . htmlspecialchars($row['name']) . "</td>";
-                            echo "<td>" . htmlspecialchars($row['mobile']) . "</td>";
-                            echo "<td>" . htmlspecialchars($row['email']) . "</td>";
+                            echo "<td>" . htmlspecialchars($row['trade_name']) . "</td>";
                             if ($row['status'] == 0) {
-                                echo "<td><span class='badge badge-pill badge-danger'>Inactive</span></td>";
+                                echo "<td><span class='badge badge-pill badge-danger'>CTS</span></td>";
                             } else {
-                                echo "<td><span class='badge badge-pill badge-success'>Active</span></td>";
+                                echo "<td><span class='badge badge-pill badge-success'>CITS</span></td>";
                             } ?>
                             </td>
                             <td><span>
                                     &nbsp;&nbsp;
-                                    <a href="<?php echo 'edit-admin.php?id=' . $row['id']; ?>" class="m-r-10" data-toggle="tooltip" data-placement="top" title="Edit"><i class="fa fa-pencil color-muted m-r-10 "></i> </a>&nbsp;&nbsp;
-                                    <a onclick="return confirm('Are you sure?')" href="<?php echo 'backend/delete-admin.php?id=' . $row['id']; ?>" class="" data-toggle="tooltip" data-placement="top" title="Delete"><i class="fa fa-close color-danger"></i></a>
+                                    <a href="<?php echo 'edit-trade.php?id=' . $row['id']; ?>" class="m-r-10" data-toggle="tooltip" data-placement="top" title="Edit"><i class="fa fa-pencil color-muted m-r-10 "></i> </a>&nbsp;&nbsp;
+                                    <a onclick="return confirm('Are you sure?')" href="<?php echo 'backend/delete-trade.php?id=' . $row['id']; ?>" class="" data-toggle="tooltip" data-placement="top" title="Delete"><i class="fa fa-close color-danger"></i></a>
                                 </span></td>
 
                             </tr>
